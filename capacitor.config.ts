9cd770414efe22fdb2e473d8f050bcc450afbee6
Darren <58746026+Dazz563@users.noreply.github.com>
2023-06-10 07:12:41 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'auth-flow',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
