c08a9811624b982b5c320d055b9f335e6132f747
Darren <58746026+Dazz563@users.noreply.github.com>
2023-06-20 10:50:41 +0200

    tabs setup complete

--- src/app/pages/menu/entertainment/entertainment-routing.module.ts
+++ src/app/pages/menu/entertainment/entertainment-routing.module.ts
@@ -1,17 +1,40 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { EntertainmentPage } from './entertainment.page';
+import {EntertainmentPage} from './entertainment.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: EntertainmentPage
-  }
+	{
+		path: '',
+		redirectTo: 'music',
+		pathMatch: 'full',
+	},
+	{
+		path: '',
+		component: EntertainmentPage,
+		children: [
+			{
+				path: 'music',
+				loadChildren: () => import('./music/music.module').then((m) => m.MusicPageModule),
+			},
+			{
+				path: 'radio',
+				loadChildren: () => import('./radio/radio.module').then((m) => m.RadioPageModule),
+			},
+			{
+				path: 'library',
+				loadChildren: () => import('./library/library.module').then((m) => m.LibraryPageModule),
+			},
+			{
+				path: 'search',
+				loadChildren: () => import('./search/search.module').then((m) => m.SearchPageModule),
+			},
+		],
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class EntertainmentPageRoutingModule {}
